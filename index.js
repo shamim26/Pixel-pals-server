ec7111fed1de6c34ce747fe0914ee639ee243101
shamim26 <shamimreja101@gmail.com>
2023-05-19 19:52:45 +0600

    put method implemented

--- index.js
+++ index.js
@@ -74,6 +74,22 @@ async function run() {
       res.send(result);
     });
 
+    // update data
+    app.put('/updateToys/:id', async (req, res) =>{
+      const id = req.params.id
+      const body = req.body;
+      const filter = {_id: new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          price: body.price,
+          availableQuantity: body.availableQuantity,
+          description: body.description,
+        },
+      };
+      const result = await toysCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
